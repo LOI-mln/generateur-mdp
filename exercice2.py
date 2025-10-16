5488b7101a2dd9fb2d6391d4bef40fa8a32e5aee
LOI-mln <milan.loi@etu.unilim.fr>
2025-10-16 15:20:36 +0200

    try except for error in the lenght input

--- exercice2.py
+++ exercice2.py
@@ -12,7 +12,18 @@ def mdp_hash(master,tag,n):
 if __name__ == "__main__":
     master = input("Enter the master password: ")
     tag = input("Enter the tag: ")
-    n = int(input("Enter the length of the generated password: "))
-    while n <= 0 or n > 12:
-        n = int(input("Please enter a valid length (1-12): "))
-    print("Generated password:", mdp_hash(master, tag, n))
+    is_correct = False
+    while not is_correct: 
+        try:
+            user_input = input("Entrez la longueur souhaitée pour le mot de passe (entre 1 et 12) : ")
+            length = int(user_input)
+
+            if 1 <= length <= 12:
+                print(f"OK, la longueur sera de {length} caractères.")
+                is_correct = True
+            else:
+                print("Erreur : Le nombre doit être compris entre 1 et 12.")
+                
+        except ValueError:
+            print("Erreur : Veuillez entrer un nombre valide.")
+    print("Generated password:", mdp_hash(master, tag, length))
