56c1e628bdbb021a059627fd0eeabb84bd4ac66a
LOI-mln <milan.loi@etu.unilim.fr>
2025-10-16 15:12:33 +0200

    using local variable n for the lenght

--- exercice2.py
+++ exercice2.py
@@ -1,15 +1,16 @@
 import hashlib
 import base64
 
-def mdp_hash(master,tag): 
+def mdp_hash(master,tag,n): 
     combined = master + tag
     bite_combined = combined.encode('utf-8')
     hash_object = hashlib.sha256(bite_combined).hexdigest()
-    final_password = hash_object[:8]
+    final_password = hash_object[:n]
     return final_password
 
 
 if __name__ == "__main__":
     master = input("Enter the master password: ")
     tag = input("Enter the tag: ")
-    print("Generated password:", mdp_hash(master, tag))
+    n = int(input("Enter the length of the generated password: "))
+    print("Generated password:", mdp_hash(master, tag, n))
